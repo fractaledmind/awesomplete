2cdbab9bed9fc8e046b0e3e4a8a1c10290891dd2
Stephen Margheim <stephen.margheim@gmail.com>
2016-01-29 16:09:45 -0500

    Handle two types of functions for list option
    a function with no args (a thunk) that simply returns a source array or a function that takes one arg (the input value) and returns a suggestions array.

--- awesomplete.js
+++ awesomplete.js
@@ -115,8 +115,11 @@ _.prototype = {
 		else if (typeof list === "string" && list.indexOf(",") > -1) {
 				this._list = list.split(/\s*,\s*/);
 		}
-		else if (typeof list === "function") {
-				this._list = list();
+		else if ($.isFunction(list) && list.length == 0) {
+			this._list = list();
+		}
+		else if ($.isFunction(list) && list.length > 0) {
+			this._list = list('');
 		}
 		else { // Element or CSS selector
 			list = $(list);
